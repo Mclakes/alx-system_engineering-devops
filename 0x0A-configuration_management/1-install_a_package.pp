6eb4552d3fe9fe55a6f2341c95f1d214ee15ebf0
mclakes <lekanhayo@gmail.com>
2024-04-27 23:11:29 -0700

    another

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-#install flask -v 2.1.0
-package { 'flask':
-	ensure => '2.1.0',
-	provider => 'pip3'
+# Install Flask using pip3
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip']
 }
